8c0a74b2d56c80c1b5dcd81984d7ee8e20fad138
Ishay1997 <74185897+Ishay1997@users.noreply.github.com>
2021-04-21 14:11:25 +0300

    Update NumberWithUnits.hpp

--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -22,15 +22,12 @@ namespace ariel
 
         static void read_units(std::ifstream &units_file);
 
-        // static double getValue(NumberWithUnits& n1);
-
-        // static std::string getmUnits(NumberWithUnits& n1);
 
         friend NumberWithUnits &operator+=(NumberWithUnits &a, const NumberWithUnits &b);
         friend NumberWithUnits &operator-=(NumberWithUnits &a, const NumberWithUnits &b);
         friend NumberWithUnits operator+(const NumberWithUnits &a, const NumberWithUnits &b);
         friend NumberWithUnits operator-(const NumberWithUnits &a, const NumberWithUnits &b);
-        friend NumberWithUnits &operator+(NumberWithUnits &a); //onari
+        friend NumberWithUnits &operator+(NumberWithUnits &a); 
         friend NumberWithUnits &operator-(NumberWithUnits &a);
 
         //////////
@@ -44,10 +41,10 @@ namespace ariel
 
         //////////
 
-        friend NumberWithUnits &operator++(NumberWithUnits &a);      //prefix
-        friend NumberWithUnits operator++(NumberWithUnits &a, int); //postfix
-        friend NumberWithUnits &operator--(NumberWithUnits &a);      //prefix
-        friend NumberWithUnits operator--(NumberWithUnits &a, int); //postfix
+        friend NumberWithUnits &operator++(NumberWithUnits &a);      
+        friend NumberWithUnits operator++(NumberWithUnits &a, int); 
+        friend NumberWithUnits &operator--(NumberWithUnits &a);      
+        friend NumberWithUnits operator--(NumberWithUnits &a, int); 
 
         ////////////
 
@@ -60,4 +57,4 @@ namespace ariel
         friend std::ostream &operator<<(std::ostream &out, const NumberWithUnits &b);
         friend std::istream &operator>>(std::istream &in, const NumberWithUnits &b);
     };
-} // namespace ariel
+} 
